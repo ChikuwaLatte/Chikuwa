4a9fe488bbb96e8669130d7e00a1f3dc18853ccc
ChikuwaLatte <tatsuikd@gmail.com>
2024-09-26 01:01:05 +0900

    自動生成でmodelにコンストラクタが生えるようにする

--- core/src/main/java/com/chikuwa_latte/core/repository/db_user/Table01Dao.java
+++ core/src/main/java/com/chikuwa_latte/core/repository/db_user/Table01Dao.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 
 import com.chikuwa_latte.core.entity.db_user.Table01;
+import com.chikuwa_latte.core.entity.db_user.Table01Example;
 import com.chikuwa_latte.core.mapper.db_user.Table01Mapper;
 
 import lombok.NonNull;
@@ -16,18 +17,22 @@ public class Table01Dao {
     private final Table01Mapper table01Mapper;
 
     public Table01 find(long userId) {
-        return table01Mapper.find(userId);
+        return table01Mapper.selectByPrimaryKey(userId);
     }
 
-    public List<Table01> findAll() {
-        return table01Mapper.findAll();
+    public List<Table01> findAll(long userId) {
+        Table01Example example = new Table01Example();
+        example.createCriteria().andUserIdEqualTo(userId);
+        return table01Mapper.selectByExample(example);
     }
 
     public void create(long userId, @NonNull String memo) {
-        table01Mapper.create(userId, memo);
+        Table01 entity = new Table01(userId, memo);
+        table01Mapper.insert(entity);
     }
 
     public void update(long userId, @NonNull String memo) {
-        table01Mapper.update(userId, memo);
+        Table01 entity = new Table01(userId, memo);
+        table01Mapper.updateByPrimaryKey(entity);
     }
 }
